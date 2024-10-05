4ca44653d79e090aa7717b1a80e7835bb1779f8b
fairytaib <fairytaib@gmail.com>
2024-10-05 22:01:31 +0200

    Change of drawing behavior
    Extra - Task. Randomize the Hover effect. Add a reset button so no reload needed

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 const container = document.querySelector(".container")
-const size = prompt("How many squares per Line do you want?")
-
+let size = prompt("How many squares per Line do you want? (Between 20 - 100)")
+const resetButton = document.querySelector(".reset")
 const heights = document.body.clientHeight / parseInt(size)
 const widths = parseInt(document.body.clientWidth) / parseInt(size) -2
+const colors = ["red", "blue", "yellow", "green"]
 
-for (block = 0; block < parseInt(size) * parseInt(size); block++){
-    const block = document.createElement("div")
-    block.classList.add("division")
-    block.style.width = `${widths}px`
-    
-    container.appendChild(block)
+while (parseInt(size) < 20 || parseInt(size) > 100){
+    alert("Your Number is not permissable!")
+    size = prompt("How many squares per Line do you want? (Between 20 - 100)")
+}
+createBlocks()
+
+resetButton.addEventListener("click", () => {
+    askSize()
+    while (parseInt(size) < 20 || parseInt(size) > 100){
+        alert("Your Number is not permissable!")
+        size = prompt("How many squares per Line do you want? (Between 20 - 100)")
+    }
+    createBlocks()
+
+})
+function askSize () {
+    size = prompt("How many squares per Line do you want? (Between 20 - 100)")
 }
 
-const field = document.querySelectorAll("div.division")
+function createBlocks(){
+    for (block = 0; block < parseInt(size) * parseInt(size); block++){
+        const block = document.createElement("div")
+        block.classList.add("division")
+        block.style.width = `${widths}px`
+        
+        container.appendChild(block)
+    }
+}
 
-field.forEach(element => {
-    element.addEventListener("mouseover", () => {
-        if (element.style.backgroundColor == "red"){
-            element.style.backgroundColor = "white"
-        } else {
-            element.style.backgroundColor = "red"
-        }
-    })})
+container.addEventListener("mouseover", (event) =>{
+    let element = event.target
+    let randomColor = colors[Math.floor((Math.random() * colors.length))]
 
-
+    switch(element.className){
+        case "division":
+            element.style.backgroundColor = `${randomColor}`
+            break;
+    }
+})
