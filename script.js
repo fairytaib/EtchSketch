743f993cdfe31d8210b3d078c4247583b8e2ed24
fairytaib <fairytaib@gmail.com>
2024-10-05 21:04:34 +0200

    Learned that you just need enough divs (pixels)
    As long as Prompt Input > 20, then flexbox stretches perfectly. Just dont go below a certain range

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.querySelector(".container")
 const size = prompt("How many squares per Line do you want?")
 
+const heights = document.body.clientHeight / parseInt(size)
+const widths = parseInt(document.body.clientWidth) / parseInt(size) -2
+
 for (block = 0; block < parseInt(size) * parseInt(size); block++){
     const block = document.createElement("div")
     block.classList.add("division")
+    block.style.width = `${widths}px`
+    
     container.appendChild(block)
 }
 
@@ -17,3 +22,5 @@ field.forEach(element => {
             element.style.backgroundColor = "red"
         }
     })})
+
+
